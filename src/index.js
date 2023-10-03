650be90d0f6eb7acf9abe5dcfa82b59cddb6200e
Sara BEGACHE <sara.begache@gmail.com>
2023-10-03 22:59:22 +0100

    Completed Assignement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
 function Home() {
   return (
@@ -14,3 +16,9 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
